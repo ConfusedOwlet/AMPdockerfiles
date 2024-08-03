a2523b036eb94a0573bcb0cf8ff8b65e70494829
Rae <17814197+ConfusedOwlet@users.noreply.github.com>
2024-08-03 00:42:47 -0600

    added winestable to builddockerbase

--- buildDockerBase.sh
+++ buildDockerBase.sh
@@ -5,7 +5,8 @@ docker build --rm -f AMPDockerFileNode -t confusedowlet/ampdocker:node .
 docker build --rm -f AMPDockerFileXvfb -t confusedowlet/ampdocker:xvfb .
 docker build --rm -f AMPDockerFilePython3 -t confusedowlet/ampdocker:python3 .
 docker build --rm -f AMPDockerFileWine -t confusedowlet/ampdocker:wine .
+docker build --rm -f AMPDockerFileWineStable -t confusedowlet/ampdocker:winestable .
 docker build --rm -f AMPDockerFileMono -t confusedowlet/ampdocker:mono .
-docker build --rm -f AMPDockerFileProtontricks -t confusedowlet/ampdocker:protontricks .
+docker build --rm -f AMPDockerFileProtontricks -t confusedowlet/ampdocker:Protontricks .
 
 #docker push -a confusedowlet/ampdocker
